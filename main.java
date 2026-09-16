a072b080f4ffa6bef66f35e67115a1857df1fe23
Chungwon Cho <chungwoncho@gmail.com>
2026-09-16 15:03:34 +0900

    feat: 연습문제 1

---
+++ main.java
@@ -0,0 +1,10 @@
+package com.survivalcoding;
+
+public class cleric {
+    // 필드(field), 멤버변수(member variable), 속성(property), 전역변수
+    string name;
+    int hp;
+    int mp;
+    static final int max_hp = 50;
+    static final int min_mp = 10;
+}
